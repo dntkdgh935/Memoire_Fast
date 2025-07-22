51918a31e9fed2aabc26193fd0e3510efc113827
Rihite624 <kdong1240@gmail.com>
2025-07-22 17:51:33 +0900

    faceLogin

--- app/services/library/keyword_search_service.py
+++ app/services/library/keyword_search_service.py
@@ -1,17 +1,17 @@
-# app/services/library/keyword_search_service.py
-
-# import cx_Oracle
-import numpy as np
-import faiss
-from sklearn.preprocessing import normalize
-
-import app.core.dbConnectTemplate as dbtemp
-import oracledb
-
-dbtemp.oracle_init()
-conn=dbtemp.connect()
-
-
-
-def search_collections(query):
-    return None
+# # app/services/library/keyword_search_service.py
+#
+# # import cx_Oracle
+# import numpy as np
+# import faiss
+# from sklearn.preprocessing import normalize
+#
+# import app.core.dbConnectTemplate as dbtemp
+# import oracledb
+#
+# dbtemp.oracle_init()
+# conn=dbtemp.connect()
+#
+#
+#
+# def search_collections(query):
+#     return None
